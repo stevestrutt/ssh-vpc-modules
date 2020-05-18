81867c52333cf2a210995712a7e0a08f49bfc9d3
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-05-18 12:21:19 +0100

    readme

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "bastion_host_ip_addresses" {
-  value = module.bastion.bastion_ip_addresses
+# Single value, pending support for multiple output values in schematics_workspace_putputs data source
+output "bastion_host_ip_address" {
+  value = module.bastion.bastion_ip_addresses[0]
 }
+
+# output "bastion_host_ip_addresses" {
+#   value = module.bastion.bastion_ip_addresses
+# }
 
 output "frontend_server_host_ip_addresses" {
   value = [module.frontend.primary_ipv4_address]
