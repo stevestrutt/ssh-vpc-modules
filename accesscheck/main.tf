a7f9d056fc3c756c6abefe83d8f3f02573dea192
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2020-05-19 14:05:17 +0100

    accesscheck

--- accesscheck/main.tf
+++ accesscheck/main.tf
@@ -1,11 +1,16 @@
+locals {
+  target_count = var.ssh_accesscheck ? len(var.target_hosts) : 0
+}
+
+
 resource "null_resource" "ssh_accesscheck" {
-  count = var.ssh_accesscheck ? 1 : 0
+  count = local.target_count
 
   connection {
     bastion_host = var.bastion_host
 
     #host = "52.116.140.31"
-    host        = var.target_hosts
+    host        = var.target_hosts[count.index]
     user        = "root"
     private_key = var.ssh_private_key
   }
